eb0d6ca8c9eddd962c58e2eea90eddc357bd2d13
shankaranarayanan712 <shankarhari712@gmail.com>
2022-08-18 23:35:43 +0530

    chore: format code and code clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import Home from './container/home';
-import 'bootstrap/dist/css/bootstrap.min.css'
+/** @format */
 
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import Home from './container/home';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 
 function App() {
-  return (
-    <Home/>
-  )
-  
+	return <Home />;
 }
 
 export default App;
